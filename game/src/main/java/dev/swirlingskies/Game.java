e36db28a1cf6e5576b65d4158b5630c740371814
Caden Vize <cadenvize210@gmail.com>
2025-09-07 00:56:27 -0500

    Continued TileMap project

--- game/src/main/java/dev/swirlingskies/Game.java
+++ game/src/main/java/dev/swirlingskies/Game.java
@@ -2,10 +2,13 @@ package dev.swirlingskies;
 
 import dev.swirlingskies.entity.Entity;
 import dev.swirlingskies.graphics.Camera;
+import dev.swirlingskies.graphics.Texture;
 import dev.swirlingskies.graphics.Window;
 import dev.swirlingskies.player.Controller;
 import dev.swirlingskies.player.Player;
 import dev.swirlingskies.scene.Scene;
+import dev.swirlingskies.tile.Tile;
+import dev.swirlingskies.tile.TileMask;
 import dev.swirlingskies.tile.tiles.TestTile;
 import dev.swirlingskies.util.Input;
 import dev.swirlingskies.util.TaskManager;
@@ -16,10 +19,12 @@ import org.joml.Vector2i;
 import java.util.Objects;
 
 import static dev.swirlingskies.scene.Scene.SCENE_SIZE;
+import static dev.swirlingskies.scene.Scene.TILE_SIZE;
 import static dev.swirlingskies.util.Input.*;
 import static dev.swirlingskies.util.Time.getDeltaTime;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.glEnable;
 import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
 
 public class Game {
@@ -98,11 +103,73 @@ public class Game {
         scene.update();
         player.update();
         controller.input();
+
+        if(isKeyPressed(GLFW_KEY_SPACE)) {
+            tile++;
+            tilemap = new int[]{
+                    0,0,0,0,0,
+                    0,tile & 1,(tile >> 1) & 1,(tile >> 2) & 1,0,
+                    0,(tile >> 3) & 1,1,(tile >> 4) & 1,0,
+                    0,(tile >> 5) & 1,(tile >> 6) & 1,(tile >> 7) & 1,0,
+                    0,0,0,0,0,
+            };
+        }
     }
+
+    TileMask mask = new TileMask();
+    Texture testTexture;
 
     public void render() {
         scene.render();
         player.render();
+        if(testTexture == null) testTexture = new Texture("resources/textures/tiles/testautotile.png");
+        for(int x = 1; x < 4; x++) {
+            for(int y = 1; y < 4; y++) {
+                if(tilemap[y*5+x]==0) continue;
+                int id = mask.TILE_MASK_ID[mask8(x,y)];
+                if(x==2&&y==2)System.out.println(id);
+                float tw = 1.0f / 12f, th = 1.0f / 4f;
+                float tx = tw * ((id+1)%12), ty = 1 - (th * ((id+1)/12));
+                glColor3f(1,1,1);
+                glEnable(GL_TEXTURE_2D);
+                testTexture.bind();
+                glBegin(GL_QUADS);
+                glTexCoord2f(tx,ty); glVertex2f(x*TILE_SIZE,y*TILE_SIZE);
+                glTexCoord2f(tx+tw,ty); glVertex2f(x*TILE_SIZE+TILE_SIZE,y*TILE_SIZE);
+                glTexCoord2f(tx+tw,ty-th); glVertex2f(x*TILE_SIZE+TILE_SIZE,y*TILE_SIZE+TILE_SIZE);
+                glTexCoord2f(tx,ty-th); glVertex2f(x*TILE_SIZE,y*TILE_SIZE+TILE_SIZE);
+                glEnd();
+                testTexture.unbind();
+            }
+        }
+    }
+
+    int tile = 0;
+    int[] tilemap = {
+            0,0,0,0,0,
+            0,0,0,0,0,
+            0,0,1,0,0,
+            0,0,0,0,0,
+            0,0,0,0,0,
+    };
+
+    public boolean same(int x, int y, int nx, int ny) {
+        if (ny < 0 || ny >= 5 || nx < 0 || nx >= 5) return false;
+        return tilemap[ny*5+nx] == tilemap[y*5+x];
+    }
+
+    public int mask8(int x, int y) {
+        int m = 0;
+        if (same(x,y,x-1,y-1)) m |= 1;      // NW
+        if (same(x,y,x,y-1)) m |= 1<<1;         // N
+        if (same(x,y,x+1,y-1)) m |= 1<<2;   // NE
+        if (same(x,y,x-1,y)) m |= 1<<3;         // W
+        if (same(x,y,x+1,y)) m |= 1<<4;         // E
+        if (same(x,y,x-1,y+1)) m |= 1<<5;   // SW
+        if (same(x,y,x,y+1)) m |= 1<<6;         // S
+        if (same(x,y,x+1,y+1)) m |= 1<<7;   // SE
+        if(x==2&&y==2)System.out.println(m);
+        return m;
     }
 
     public static void main(String[] args) {
--- game/src/main/java/dev/swirlingskies/tile/TileMask.java
+++ game/src/main/java/dev/swirlingskies/tile/TileMask.java
@@ -4,39 +4,40 @@ import static dev.swirlingskies.scene.Scene.*;
 
 public class TileMask {
     public final int[] TILE_MASK_ID = {
-            0, 14, 0, 14, 28, 15, 28, 36, 0, 14, 0, 14, 28, 15, 28, 36,
-            7, 8, 7, 8, 1, 32, 1, 25, 7, 8, 7, 8, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
-            x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x,
+            0,0,1,1,0,0,1,1,2,2,3,4,2,2,3,4,
+            5,5,6,6,5,5,7,7,8,8,9,10,8,8,11,12,
+            0,0,1,1,0,0,1,1,2,2,3,4,2,2,3,4,
+            5,5,6,6,5,5,7,7,8,8,9,10,8,8,11,12,
+            13,13,14,14,13,13,14,14,15,15,16,17,15,15,16,17,
+            18,18,19,19,18,18,20,20,21,21,22,23,21,21,24,25,
+            13,13,14,14,13,13,14,14,26,26,27,28,26,26,27,28,
+            18,18,19,19,18,18,20,20,29,29,30,31,29,29,32,33,
+            0,0,1,1,0,0,1,1,2,2,3,4,2,2,3,4,
+            5,5,6,6,5,5,7,7,8,8,9,10,8,8,11,12,
+            0,0,1,1,0,0,1,1,2,2,3,4,2,2,3,4,
+            5,5,6,6,5,5,7,7,8,8,9,10,8,8,11,12,
+            13,13,14,14,13,13,14,14,15,15,16,17,15,15,16,17,
+            34,34,35,35,34,34,36,36,37,37,38,39,37,37,40,41,
+            13,13,14,14,13,13,14,14,26,26,27,28,26,26,27,28,
+            34,34,35,35,34,34,36,36,42,42,43,44,42,42,45,46
     };
 
-    public boolean same(Tile[] tiles, int x, int y, int nx, int ny) {
+    public boolean same(Tile[] tiles, int x, int y, int nx, int ny, Tile tileType) {
         if (ny < 0 || ny >= SCENE_SIZE || nx < 0 || nx >= SCENE_SIZE) return false;
-        return tiles[ny*SCENE_SIZE+nx] == tiles[y*SCENE_SIZE+x];
+        return tiles[ny*SCENE_SIZE+nx].getClass().equals(tileType.getClass());
     }
 
-    public int mask8(Tile[] tiles, int x, int y) {
+
+    public int mask8(Tile[] tiles, int x, int y, Tile tileType) {
         int m = 0;
-        if (same(tiles,x,y,x,y-1)) m |= 1;            // N
-        if (same(tiles,x,y,x+1,y-1)) m |= 1<<1;   // NE
-        if (same(tiles,x,y,x+1,y)) m |= 1<<2;         // E
-        if (same(tiles,x,y,x+1,y+1)) m |= 1<<3;   // SE
-        if (same(tiles,x,y,x,y+1)) m |= 1<<4;         // S
-        if (same(tiles,x,y,x-1,y+1)) m |= 1<<5;   // SW
-        if (same(tiles,x,y,x-1,y)) m |= 1<<6;         // W
-        if (same(tiles,x,y,x-1,y-1)) m |= 1<<7;   // NW
+        if (same(tiles,x,y,x-1,y-1,tileType)) m |= 1;      // NW
+        if (same(tiles,x,y,x,y-1,tileType)) m |= 1<<1;         // N
+        if (same(tiles,x,y,x+1,y-1,tileType)) m |= 1<<2;   // NE
+        if (same(tiles,x,y,x-1,y,tileType)) m |= 1<<3;         // W
+        if (same(tiles,x,y,x+1,y,tileType)) m |= 1<<4;         // E
+        if (same(tiles,x,y,x-1,y+1,tileType)) m |= 1<<5;   // SW
+        if (same(tiles,x,y,x,y+1,tileType)) m |= 1<<6;         // S
+        if (same(tiles,x,y,x+1,y+1,tileType)) m |= 1<<7;   // SE
         return m;
     }
 }
